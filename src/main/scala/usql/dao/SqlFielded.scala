a06e88fa2228c3edfa8191070512e4645403933f
Norbert Schultz <norbert.schultz@reactivecore.de>
2025-03-28 17:59:05 +0100

    Design

--- src/main/scala/usql/dao/SqlFielded.scala
+++ src/main/scala/usql/dao/SqlFielded.scala
@@ -12,14 +12,23 @@ trait SqlFielded[T] extends SqlColumnar[T] {
 
 /** A Field of a case class. */
 sealed trait Field[T] {
+
+  /** Name of the field (case class member) */
   def fieldName: String
+
+  /** Columns represented by this field. */
+  def columns: SqlColumns
 }
 
 object Field {
 
   /** A Field which maps to a column */
-  case class Column[T](fieldName: String, columnName: SqlIdentifier, dataType: DataType[T]) extends Field[T]
+  case class Column[T](fieldName: String, column: SqlColumn[T]) extends Field[T] {
+    override def columns: SqlColumns = SqlColumns(List(column))
+  }
 
   /** A Field which maps to a nested case class */
-  case class Group[T](fieldName: String, fielded: SqlFielded[T]) extends Field[T]
+  case class Group[T](fieldName: String, fielded: SqlFielded[T]) extends Field[T] {
+    override def columns: SqlColumns = fielded.columns
+  }
 }
